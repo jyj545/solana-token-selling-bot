0ef9668cd9668ceb4f8b8060c6176665a8f216ab
jyj545 <jyj545@vip.qq.com>
2024-05-25 11:50:46 +0000

    update

---
+++ watchtelegram/watchGmgn.py
@@ -0,0 +1,79 @@
+import requests
+import json
+import asyncio
+import aiohttp
+import time
+# 定义输出文件路径
+output_file_path = 'sniper-list.txt'
+# 定义代理的IP地址和端口
+proxy_ip = '127.0.0.1'
+proxy_port = 10809
+proxy_url = f'http://{proxy_ip}:{proxy_port}'
+
+# 异步函数，用于异步获取数据
+async def fetch_data_from_url(url, proxy):
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(url, proxy=proxy) as response:
+                response.raise_for_status()  # 如果HTTP请求返回了失败的状态码，将抛出异常
+                data = await response.json()
+                return data
+        except aiohttp.ClientError as e:
+            print(f"请求失败: {e}")
+            return None
+
+# 异步函数，用于将新发现的Solana地址写入文件
+async def write_new_address(new_address):
+    try:
+        with open(output_file_path, 'r', encoding='utf-8') as file:
+            known_addresses = set(line.strip().split('/')[0] for line in file.readlines())
+    except FileNotFoundError:
+        known_addresses = set()
+
+    if new_address.strip().split('/')[0] not in known_addresses:
+        known_addresses.add(new_address)
+        with open(output_file_path, 'a', encoding='utf-8') as file:
+            file.write(new_address + '\n')
+
+
+# 异步主函数
+async def main():
+    try:
+        url = 'https://gmgn.ai/defi/quotation/v1/pairs/sol/new_pairs?limit=10&orderby=open_timestamp&direction=desc&filters[]=not_honeypot'
+        data = await fetch_data_from_url(url, proxy_url)
+        if data:
+            for token in data['data']['pairs']:
+                print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+                print('name:%f',token['base_token_info']['name'])
+                print('liquidity:%f',token['liquidity'])
+                print('top_10_holder_rate:%f',token['base_token_info']['top_10_holder_rate'])
+                print('burn_status:%f',token['base_token_info']['burn_status'])
+                print('is_honeypot:%f',token['base_token_info']['is_honeypot'])
+                print('renounced_mint:%f',token['base_token_info']['renounced_mint'])
+                print('launchpad:%f',token['launchpad'])
+                if token['base_token_info']['top_10_holder_rate'] is None:
+                    # 如果top_10_holder_rate为None，可以选择跳过这个token或者赋予一个默认值
+                    continue  # 跳过这个token
+                if float(token['liquidity'])>1000 and float(token['base_token_info']['top_10_holder_rate'])<0.3 and token['base_token_info']['burn_status']=='burn' and token['base_token_info']['is_honeypot'] is None and token['base_token_info']['renounced_mint']==1 and token['launchpad'] != 'Pump.fun':
+                # if float(token['liquidity'])>1 and float(token['base_token_info']['top_10_holder_rate'])<0.9 and token['base_token_info']['burn_status']=='burn' and token['base_token_info']['is_honeypot'] is None and token['base_token_info']['renounced_mint']==1:
+                        
+                    url1='https://gmgn.ai/defi/quotation/v1/tokens/stats/sol/'+token['base_token_info']['address']
+                    data1 = await fetch_data_from_url(url1, proxy_url)
+                    if float(data1['data']['top_rat_trader_amount_percentage'])>0.2:
+                        # 如果top_10_holder_rate为None，可以选择跳过这个token或者赋予一个默认值
+                        continue  # 跳过这个token
+                    url2='https://gmgn.ai/defi/quotation/v1/tokens/sol/'+token['base_token_info']['address']
+                    data2 = await fetch_data_from_url(url2, proxy_url)
+                    if data2['data']['token']['rug_ratio'] is not None:
+                        # 如果top_10_holder_rate为None，可以选择跳过这个token或者赋予一个默认值
+                        continue  # 跳过这个token
+                    await write_new_address(token['base_token_info']['address']+'/'+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+    except Exception as e:
+        print(e)
+    
+
+# 启动异步事件循环
+if __name__ == '__main__':
+    while True:
+        asyncio.run(main())
+        time.sleep(5)
---
+++ watchtelegram/watchTweets.py
@@ -0,0 +1,103 @@
+import os
+import time
+import asyncio
+from typing import List, NoReturn, Optional
+import httpx
+from twikit import Client, Tweet
+import re
+
+# 正则表达式模式
+address_pattern = r'\b[A-Za-z0-9]{44}\b'
+
+# 认证信息，请根据实际情况进行替换
+AUTH_INFO_1 = "jyj545"
+AUTH_INFO_2 = 'jyj545@vip.qq.com'
+PASSWORD = "Jyj4531545"
+
+class TwitterUser:
+    def __init__(self, screen_name: str, user_id: int = None):
+        self.screen_name = screen_name
+        self.user_id = user_id
+        self.latest_tweet = None
+
+    def fetch_user_info(self, client: Client) -> None:
+        """获取并更新用户的ID和最新消息"""
+        try:
+            user_info = client.get_user_by_screen_name(self.screen_name)
+            self.user_id = user_info.id
+            self.latest_tweet = client.get_user_tweets(self.user_id, 'Replies')[0]
+        except IndexError:
+            return None
+        except httpx.ConnectTimeout as e:
+            print(f"请求超时：{e}")
+
+class TwitterListener:
+    def __init__(self, screen_names: List[str]):
+        self.users = [TwitterUser(screen_name) for screen_name in screen_names]
+        self.client = Client('en-US', timeout=60)
+        self.check_interval = 60  # 每1分钟检查一次
+        self.known_addresses = set()
+        self.output_file_path = 'new_solana_addresses.txt'
+
+    def load_or_login(self):
+        """加载cookies或登录并保存cookies"""
+        if os.path.exists('cookies.json'):
+            print("Loading cookies...")
+            self.client.load_cookies('cookies.json')
+        else:
+            print("Logging in and saving cookies...")
+            self.client.login(
+                auth_info_1=AUTH_INFO_1,
+                auth_info_2=AUTH_INFO_2,
+                password=PASSWORD
+            )
+            self.client.save_cookies('cookies.json')
+
+    def load_known_addresses(self):
+        try:
+            with open('known_addresses.txt', 'r', encoding='utf-8') as file:
+                self.known_addresses = set([line.strip() for line in file.readlines()])
+        except FileNotFoundError:
+            print("未找到已知地址文件，创建一个空的地址集合。")
+
+    def write_new_address(self, new_address):
+        self.known_addresses.add(new_address)
+        with open(self.output_file_path, 'a', encoding='utf-8') as file:
+            file.write(new_address + '\n')
+
+    def callback(self, tweet: Tweet) -> None:
+        print(f'New tweet posted: {tweet.text}')
+        matches = re.findall(address_pattern, tweet.text)
+        for match in matches:
+            print(f"Found Solana address: {match}")
+            if match not in self.known_addresses:
+                self.write_new_address(match)
+
+    def listen(self):
+        self.load_or_login()
+        for user in self.users:
+            user.fetch_user_info(self.client)
+
+        self.load_known_addresses()
+
+        while True:
+            time.sleep(self.check_interval)
+            for user in self.users:
+                new_tweet = self.get_latest_tweet(user.user_id)
+                if new_tweet != user.latest_tweet:
+                    self.callback(new_tweet)
+                    user.latest_tweet = new_tweet
+
+    def get_latest_tweet(self, user_id: int) -> Optional[Tweet]:
+        try:
+            tweets = self.client.get_user_tweets(user_id, 'Replies')
+            return tweets[0] if tweets else None
+        except IndexError:
+            return None
+        except httpx.ConnectTimeout as e:
+            print(f"请求超时：{e}")
+
+if __name__ == "__main__":
+    screen_names = ['eth200000', 'Crypto_Shuiyi','PepeBoost888']
+    listener = TwitterListener(screen_names)
+    listener.listen()
